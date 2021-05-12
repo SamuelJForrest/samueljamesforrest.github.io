37eb072f893f006a54cd52a77187e797c5000587
SamuelJForrest <samueljamesforrestweb@gmail.com>
2021-05-12 21:17:50 +0100

    Add files via upload
    Added an animation to info text, and a bottom slider containing links to Twitter and GitHub profiles.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('test');
+const socialSlider = document.querySelector('.social-slider');
+const socials = document.querySelector('.socials');
+const arrowSlider = document.querySelector('.arrow');
+
+socialSlider.addEventListener('click', () => {
+    socials.classList.toggle('slide-up');
+    arrowSlider.classList.toggle('arrow-rotate');
+});
