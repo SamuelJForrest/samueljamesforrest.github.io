c567a71c91b299ca24a4bb7f769ba9b50aaf4984
SamuelJForrest <samueljamesforrestweb@gmail.com>
2021-05-14 20:56:58 +0100

    Add files via upload
    Added:
    • Animations to main links
    • Projects section consisting of cards to populate later
    • More responsive aspects for different screen sizes.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// define and add event listener to display socials bar (big resolutions only)
 const socialSlider = document.querySelector('.social-slider');
 const socials = document.querySelector('.socials');
 const arrowSlider = document.querySelector('.arrow');
@@ -6,3 +8,23 @@ socialSlider.addEventListener('click', () => {
     socials.classList.toggle('slide-up');
     arrowSlider.classList.toggle('arrow-rotate');
 });
+
+// Define navbar elements to make responsive
+const burgerButton = document.querySelector('.burger-button');
+const topLinks = document.querySelector('.links');
+const lineOne = document.querySelector('.line-one');
+const lineTwo = document.querySelector('.line-two');
+const lineThree = document.querySelector('.line-three');
+const homeLink = document.querySelector('.home-link');
+const projectLink = document.querySelector('.project-link');
+const contactLink = document.querySelector('.contact-link');
+
+// Event listener to reveal navbar links
+
+burgerButton.addEventListener('click', () => {
+    topLinks.classList.toggle('show-nav');
+    lineOne.classList.toggle('line-one-rotate');
+    lineTwo.classList.toggle('line-two-remove');
+    lineThree.classList.toggle('line-three-rotate');
+});
+
