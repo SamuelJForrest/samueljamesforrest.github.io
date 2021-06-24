5930ed2b05307a7646440f6e3c9fa43253b2fccf
SamuelJForrest <samueljamesforrestweb@gmail.com>
2021-06-24 21:00:02 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const homeLink = document.querySelector('.home-link');
 const projectLink = document.querySelector('.project-link');
 const contactLink = document.querySelector('.contact-link');
 
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const projectImg = document.querySelector('.project-img');
+const projectName = document.querySelector('.project-name');
+const projectDescription = document.querySelector('.project-description');
+
 // Event listener to reveal navbar links
 
 burgerButton.addEventListener('click', () => {
@@ -28,14 +34,53 @@ burgerButton.addEventListener('click', () => {
     lineThree.classList.toggle('line-three-rotate');
 });
 
+
+let counter = 0;
+
+nextBtn.addEventListener('click', () => {
+    counter++;
+    if (counter > projects.length - 1){
+        counter = 0;
+    }
+    projectImg.src = projects[counter].image;
+    projectName.textContent = projects[counter].name;
+    projectDescription.textContent = projects[counter].description;
+});
+
+prevBtn.addEventListener('click', () => {
+    counter--;
+    if (counter < 0){
+        counter = projects.length - 1;
+    }
+    projectImg.src = projects[counter].image;
+    projectName.textContent = projects[counter].name;
+    projectDescription.textContent = projects[counter].description;
+});
+
 // Projects object
 
 const projects = [
     {
-        name: '',
-        description: '',
-        languages: '',
-        image: '',
+        name: 'Calculator',
+        description: 'This is a calculator',
+        languages: 'HTML - CSS - JavaScript',
+        image: './img/Projects/calculator app.png',
+        sitelink: '',
+        gitlink: ''
+    },
+    {
+        name: 'Weather App',
+        description: 'This is a weather app',
+        languages: 'HTML - CSS - JavaScript',
+        image: './img/Projects/Weather API app.png',
+        sitelink: '',
+        gitlink: ''
+    },
+    {
+        name: 'To-Do list',
+        description: 'This is a to-do list',
+        languages: 'HTML - CSS - JavaScript',
+        image: './img/Projects/To do list.png',
         sitelink: '',
         gitlink: ''
     }
