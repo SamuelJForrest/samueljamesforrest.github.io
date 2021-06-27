5baae0568ec4360eabcf6fa45cb960d817324644
SamuelJForrest <samueljamesforrestweb@gmail.com>
2021-06-27 13:26:44 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ const prevBtn = document.querySelector('.prev-btn');
 const projectImg = document.querySelector('.project-img');
 const projectName = document.querySelector('.project-name');
 const projectDescription = document.querySelector('.project-description');
+const projectTech = document.querySelector('.project-tech');
+const gitLink = document.querySelector('.git-link');
+const siteLink = document.querySelector('.site-link');
 
 // Event listener to reveal navbar links
 
@@ -42,9 +45,38 @@ nextBtn.addEventListener('click', () => {
     if (counter > projects.length - 1){
         counter = 0;
     }
+    projectTech.textContent = '';
     projectImg.src = projects[counter].image;
     projectName.textContent = projects[counter].name;
     projectDescription.textContent = projects[counter].description;
+    gitLink.href = projects[counter].gitlink;
+    siteLink.href = projects[counter].sitelink;
+
+        if (projects[counter].languages.includes('HTML')){
+            let htmlLogo = document.createElement('img');
+            htmlLogo.src = './img/html5.svg';
+            projectTech.append(htmlLogo);
+        }
+
+        if (projects[counter].languages.includes('CSS')){
+            let cssLogo = document.createElement('img');
+            cssLogo.src = './img/css3.svg';
+            projectTech.append(cssLogo);
+        }
+
+        if (projects[counter].languages.includes('SCSS')){
+            let sassLogo = document.createElement('img');
+            sassLogo.src = './img/sass.svg';
+            projectTech.append(sassLogo);
+        }
+
+        if (projects[counter].languages.includes('JS')){
+            let jsLogo = document.createElement('img');
+            jsLogo.src = './img/javascript.svg';
+            projectTech.append(jsLogo);
+        }
+
+        
 });
 
 prevBtn.addEventListener('click', () => {
@@ -52,9 +84,34 @@ prevBtn.addEventListener('click', () => {
     if (counter < 0){
         counter = projects.length - 1;
     }
+    projectTech.textContent = '';
     projectImg.src = projects[counter].image;
     projectName.textContent = projects[counter].name;
     projectDescription.textContent = projects[counter].description;
+
+    if (projects[counter].languages.includes('HTML')){
+        let htmlLogo = document.createElement('img');
+        htmlLogo.src = './img/html5.svg';
+        projectTech.append(htmlLogo);
+    }
+
+    if (projects[counter].languages.includes('CSS')){
+        let cssLogo = document.createElement('img');
+        cssLogo.src = './img/css3.svg';
+        projectTech.append(cssLogo);
+    }
+
+    if (projects[counter].languages.includes('SCSS')){
+        let sassLogo = document.createElement('img');
+        sassLogo.src = './img/sass.svg';
+        projectTech.append(sassLogo);
+    }
+
+    if (projects[counter].languages.includes('JS')){
+        let jsLogo = document.createElement('img');
+        jsLogo.src = './img/javascript.svg';
+        projectTech.append(jsLogo);
+    }
 });
 
 // Projects object
@@ -63,26 +120,26 @@ const projects = [
     {
         name: 'Calculator',
         description: 'This is a calculator',
-        languages: 'HTML - CSS - JavaScript',
+        languages: ['HTML', 'SCSS', 'JS'],
         image: './img/Projects/calculator app.png',
-        sitelink: '',
-        gitlink: ''
+        sitelink: 'https://trusting-hypatia-918c3a.netlify.app/',
+        gitlink: 'https://github.com/SamuelJForrest/Calculator'
     },
     {
         name: 'Weather App',
         description: 'This is a weather app',
-        languages: 'HTML - CSS - JavaScript',
+        languages: ['HTML', 'CSS', 'JS'],
         image: './img/Projects/Weather API app.png',
-        sitelink: '',
-        gitlink: ''
+        sitelink: 'https://loving-galileo-ea6a7c.netlify.app/',
+        gitlink: 'https://github.com/SamuelJForrest/weather-app'
     },
     {
         name: 'To-Do list',
         description: 'This is a to-do list',
-        languages: 'HTML - CSS - JavaScript',
+        languages: ['HTML', 'SCSS', 'JS'],
         image: './img/Projects/To do list.png',
-        sitelink: '',
-        gitlink: ''
+        sitelink: 'https://silly-murdock-31fb98.netlify.app/',
+        gitlink: 'https://github.com/SamuelJForrest/to-do-list-v2'
     }
 ];
 
